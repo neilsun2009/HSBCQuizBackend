bb2896e1431acf3f1ea3fb8391819c4ffe901f25
neilsun2009 <neilsun2--0@163.com>
2023-07-06 05:49:34 +0000

    [update][app] add static support

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, g, jsonify, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__,
+    static_url_path='/public',
+    static_folder='static',
+    template_folder='static',
+    instance_relative_config=True,
+)
 
-@app.route("/")
+@app.route("/api/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+    # return render_template('index.html')
