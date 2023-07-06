7fe404cfa1d2872bd55119ee1d37de54544832bb
neilsun2009 <neilsun2--0@163.com>
2023-07-06 07:47:16 +0000

    [update][app] use blob storage for frontend static

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request, g, jsonify, send_from_directory
+from flask import Flask, make_response, request, jsonify
 import os
+import requests
+
+STATIC_URL_PREFIX = 'https://szbstorage.z20.web.core.windows.net/'
 
 app = Flask(__name__,
-    static_url_path='/public',
-    static_folder='static',
-    template_folder='static',
+    # static_url_path='https://szbstorage.z20.web.core.windows.net/',
+    # static_folder='static',
+    # template_folder='static',
     instance_relative_config=True,
 )
 
@@ -15,8 +18,12 @@ def hello_world():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
+    # if path != "" and os.path.exists(app.static_folder + '/' + path):
+    #     return send_from_directory(app.static_folder, path)
+    # else:
+    #     return send_from_directory(app.static_folder, 'index.html')
     # return render_template('index.html')
+    res = requests.get(STATIC_URL_PREFIX + path)
+    response = make_response(res.content)
+    response.headers = dict(res.headers)
+    return response
