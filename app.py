94845fbf0323bc4d1b455a81ea7a5fedb2759599
neilsun2009 <neilsun2--0@163.com>
2023-07-06 13:22:41 +0000

    [add][comment] auto completion

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import os
 import requests
 
 STATIC_URL_PREFIX = 'https://szbstorage.z20.web.core.windows.net/'
+HUGGINGFACE_BEARER = os.environ.get('HF_BEARER')
+HUGGINGFACE_MODEL_ID = 'neilsun2009/amz_movie_tv_distilgpt2_1k'
+HUGGINGFACE_API_URL = f"https://api-inference.huggingface.co/models/{HUGGINGFACE_MODEL_ID}"
 
 app = Flask(__name__,
     # static_url_path='https://szbstorage.z20.web.core.windows.net/',
@@ -11,9 +14,37 @@ app = Flask(__name__,
     instance_relative_config=True,
 )
 
-@app.route("/api/hello")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/api/comments/auto_completion", methods=('GET', ))
+def get_auto_completion_comment():
+    comment = request.args.get('comment', '')
+    result = {
+        'comment': '',
+        'isLoading': True,
+    }
+    if comment != '':
+        res = requests.post(HUGGINGFACE_API_URL, 
+            headers={
+                "Authorization": f'Bearer {HUGGINGFACE_BEARER}'
+            }, 
+            json={
+                "inputs": comment,
+                # these params actually are not useable for PEFT models
+                "parameters": {
+                    "return_full_text": False,
+                    "temperature": 5.0,
+                    "repetition_penalty": 10.0,
+                    "top_k": 5
+                }
+            }
+        )
+        json_result = res.json()
+        print(json_result)
+        if res.status_code == 200:
+            result = {
+                'comment': json_result[0].strip(),
+                'isLoading': False,
+            }
+    return jsonify(result)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
